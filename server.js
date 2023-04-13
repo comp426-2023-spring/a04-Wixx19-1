db1a3ff464366d2c926fc44778be3ec194ce828c
William Garcia-Sanchez <89278772+Wixx19@users.noreply.github.com>
2023-04-13 00:32:09 -0400

    rework

--- server.js
+++ server.js
@@ -1,62 +1,61 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
-import express from 'express';
+import minimist from "minimist";
+import express from "express";
 
-import { playRps, playRpsls } from './lib/rpsls.js'
+import { playRps, playRpsls} from "./lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
+const app = express();
 const port = args["port"] || 5000
-const app = express(); 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 
-//get 
-app.get('/app', (req, res) => {
+app.get("/app/", (req, res) => {
     res.status(200).send('200 OK');
 })
 
-app.get('/app/rps', (req,res) => {
+app.get("/app/rps/", (req, res) => {
     res.status(200).send(JSON.stringify(playRps()));
 })
 
-app.get('/app/rpsls', (req,res) => {
+app.get("/app/rpsls/", (req, res) => {
     res.status(200).send(JSON.stringify(playRpsls()));
 })
 
-//Queries
-app.get('/app/rps/play', (req,res) => {
+//queries
+app.get("/app/rps/play/", (req, res) => {
     res.status(200).send(JSON.stringify(playRps(req.query.shot)));
 })
 
-app.get('/app/rpsls/play', (req,res) => {
+app.get("/app/rpsls/play/", (req, res) => {
     res.status(200).send(JSON.stringify(playRpsls(req.query.shot)));
 })
 
-//JSON bodies
-app.post('/app/rps/play', (req,res) => {
+// accept JSON request bodies
+app.post("/app/rps/play/", (req, res) => {
     res.status(200).send(JSON.stringify(playRps(req.body.shot)));
 })
 
-app.post('/app/rpsls/play', (req,res) => {
+app.post("/app/rpsls/play/", (req, res) => {
     res.status(200).send(JSON.stringify(playRpsls(req.body.shot)));
 })
 
-//parameters endpoints
-app.get('/app/rps/play/:shot', (req,res) => {
-    res.status(200).send(JSON.stringify(playRps(req.params.shot))).end();
+//url (parameter) endpoint
+app.get("/app/rps/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(playRps(req.params.shot)));
 })
 
-app.get('/app/rpsls/play/:shot', (req,res) => {
-    res.status(200).send(JSON.stringify(playRpsls(req.params.shot))).end();
+app.get("/app/rpsls/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(playRpsls(req.params.shot)));
 })
 
-app.get('*', (req,res) => {
-    res.status(404).send('404 NOT FOUND').end();
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
 })
 
-app.listen(port, () => {
-    console.log('Server listening on port' + port);
+app.listen(port, () =>{
+    console.log('Server listening on port ' + port);
 })
